d67e91704b25bb92ebfe3ad5995eee5fbb311906
Jishnu Kumar Srivastava <jisheras31@gmail.com>
2023-01-17 15:54:19 +0530

    More Learnings!

--- HTML&CSS/Learning1.js
+++ HTML&CSS/Learning1.js
@@ -1,5 +1,8 @@
+/*function startListen(){ * }
+document.getElementsById('demo').addEventListener('click',myFunction)
 function myFunction() 
 {
+
     const numbers1 = [45, 4, 9, 16, 25,-30];
     //const numbers2 = numbers1.map(myFunction);
     let suii=0;
@@ -7,6 +10,22 @@ function myFunction()
         suii += numbers1[i];
         
     }
-    return suii;
+    document.getElementById('demo').innerHTML=suii;
 }
-document.getElementById('demo').innerHTML = myFunction();
+//document.getElementById('demo').innerHTML = myFunction();*/
+
+window.onload=function(){
+    var buttonElement = document.getElementById("demo");
+    if (buttonElement){
+        buttonElement.addEventListener('click',myFunction);
+    }
+
+    function myFunction(){
+        const numbers1=[45,4,9,16,25,-30];
+        let suii=0;
+        for(let i=0;i<numbers1.length;i++){
+            suii+=numbers1[i];
+        }
+        buttonElement.innerHTML=suii;
+    }
+}
