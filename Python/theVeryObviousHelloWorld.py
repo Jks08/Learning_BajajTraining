0d880a2a8e29fb93c4ec3bc974693905afcbd9c0
Jishnu Kumar Srivastava <jisheras31@gmail.com>
2023-01-18 11:02:16 +0530

    Created pyc file.

--- Python/theVeryObviousHelloWorld.py
+++ Python/theVeryObviousHelloWorld.py
@@ -1,2 +1,12 @@
+from collections import * 
+import pandas as pd
+import numpy as np
+import time
+import os
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
+import keras
+
+
 name = input('Enter Name: ')
-print(f'Hello {name}. Nice to meet you.')
+print(f'Hello {name}. Nice to meet you. Good Morning!')
