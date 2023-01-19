e9c36be51da3a061757307cc34976402b0362232
Jishnu Kumar Srivastava <jisheras31@gmail.com>
2023-01-19 11:58:58 +0530

    Default values set.

--- Python/Learning2.py
+++ Python/Learning2.py
@@ -1,7 +1,7 @@
 # from Learning1 import *
 import testImportFile as tif
 class Emp:
-    def __init__(self,eid,ename,esal=70000):    #self refers to current object. A name other than self can be given, 
+    def __init__(self,eid='xxxxx',ename='Jon Doe',esal=29000):    #self refers to current object. A name other than self can be given, 
         self.__eid = eid                          # but the same must be referred throughout.
         self.__ename=ename
         self.__esal=esal
@@ -13,7 +13,7 @@ class Emp:
         print(f"Employee name is {self.__ename} bearing ID {self.__eid} and earning {self.__esal} Rs monthly.")
 
 if __name__=='__main__':
-    obj1 = Emp(12345,'AB',60000)  #obj is a pointer to the actual object, which is Emp(..). First, allocation of memory happens, 
+    obj1 = Emp()  #obj is a pointer to the actual object, which is Emp(..). First, allocation of memory happens, 
     obj1.display()                # then init function is called. init stand for initialize
     print()                
     obj1.disp2()                  
