2de9f04293714f8a7e45c611657fc694cf725064
Jishnu Kumar Srivastava <jisheras31@gmail.com>
2023-01-24 17:11:51 +0530

    More OOPS in JS

--- Javascript/Learning1.js
+++ Javascript/Learning1.js
@@ -31,3 +31,54 @@ class Cars{
 
 let myCar = new Cars("Ford", 2014);
 console.log("My car is " + myCar.age() + " years old");
+
+class Emp{
+    static company = "Bajaj Markets"; //static variable and these must be static. static can be called by class name.
+    constructor(eid,ename,esal){
+        this.eid=eid;
+        this.ename=ename;
+        this.esal=esal;
+    }
+    set emp_id(eid){this.eid=eid;}
+    set emp_name(ename){this.ename=ename;}
+    set emp_sal(esal){this.esal=esal;}
+    get emp_id(){return this.eid;}
+    get emp_name(){return this.ename;}
+    get emp_sal(){return this.esal;}
+
+    disp(){
+        console.log("Employee ID: "+this.eid);
+        console.log("Employee Name: "+this.ename);
+        console.log("Employee Salary: "+this.esal);
+    }
+
+    static get_company(){
+        return Emp.company;
+    }
+}
+
+let e1 = new Emp("E001","John Doe",60000);
+console.log(e1.disp(),Emp.get_company());
+console.log(e1.eid,e1.ename,e1.esal);
+e1.eid="E002";
+e1.ename="Jane Doe";
+e1.esal=70000;
+console.log(e1.eid,e1.ename,e1.esal);
+Emp.company = "Bajaj Markets Pvt. Ltd.";
+console.log(Emp.get_company());
+
+class Meetup{
+    constructor(organizer){
+        this.organizer = organizer;
+    }
+}
+
+class TeachMeet extends Meetup{
+    constructor(organizer,topic){
+        super(organizer);
+        this.topic = topic;
+    }
+}
+
+let tm = new TeachMeet("John Doe","Javascript");
+console.log(tm.organizer,tm.topic);
