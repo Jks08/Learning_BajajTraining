5b626b34c36e3739ed6d218e65d52d4dde9ace84
Jishnu Kumar Srivastava <jisheras31@gmail.com>
2023-01-25 12:33:37 +0530

    Made a calculator in Java.

---
+++ Calculator.java
@@ -0,0 +1,35 @@
+// Program for a calculator in Java:
+
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the first number: ");
+        int num1 = sc.nextInt();
+        System.out.println("Enter the second number: ");
+        int num2 = sc.nextInt();
+        System.out.println("Enter the operator: ");
+        char op = sc.next().charAt(0);
+        int result = 0;
+        switch (op) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                result = num1 / num2;
+                break;
+            default:
+                System.out.println("Invalid Operator");
+        }
+        System.out.println("The result is: " + result);
+        sc.close();
+    }
+}
+
