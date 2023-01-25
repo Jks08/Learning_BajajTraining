eeb24544bb4c5e557c34fecf1a65f4c90988864d
Jishnu Kumar Srivastava <jisheras31@gmail.com>
2023-01-25 12:30:18 +0530

    First try at Java.

---
+++ HelloWorld.java
@@ -0,0 +1,53 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello JKS. Welcome to Java World! This is the stuff of nightmares.");
+    }
+}
+
+// Java Basics:
+// 1. Java is a compiled language. You need to compile your code before you can run it.
+// 2. Java is a strongly typed language. You need to declare the type of variables.
+// 3. Java is an object oriented language. Everything is an object.
+// 4. Java is a platform independent language. You can run the same code on any platform.
+// 5. Java is a case sensitive language. HelloWorld and helloWorld are two different classes.
+// 6. Java is a multi-threaded language. You can run multiple threads in parallel.
+// 7. Java is a garbage collected language. You don't need to worry about memory management.
+// 8. Java is a secure language. You can't access the file system directly. You need to use APIs.
+// 9. Java is a distributed language. You can write distributed applications using Java.
+// 10. Java is a high performance language. It is faster than C++.
+
+// Java Keywords:
+// abstract, assert, boolean, break, byte, case, catch, char, class, const, continue, default, do, double, else, enum, extends, final, finally, float, for, goto, if, implements, import, instanceof, int, interface, long, native, new, package, private, protected, public, return, short, static, strictfp, super, switch, synchronized, this, throw, throws, transient, try, void, volatile, while
+
+// Java Identifiers:
+// 1. Identifiers are names given to classes, variables, methods, and packages.
+// 2. Identifiers can be a combination of letters, digits, underscores, and dollar signs.
+// 3. Identifiers cannot start with a digit.
+// 4. Identifiers cannot be a keyword.
+// 5. Identifiers are case sensitive.
+// 6. Identifiers should be meaningful.
+// 7. Identifiers should not be too long.
+// 8. Identifiers should not contain spaces.
+
+// Java Naming Conventions:
+// 1. Class names should start with a capital letter. For example: HelloWorld.
+// 2. Method names should start with a lower case letter. For example: main.
+// 3. Variable names should start with a lower case letter. For example: args.
+// 4. Package names should be in all lower case. For example: com.jks.helloworld.
+// 5. Constant names should be in all upper case. For example: PI.
+
+// Java Comments:
+// 1. Single line comments start with // and end with a new line.
+// 2. Multi line comments start with /* and end with */.
+// 3. JavaDoc comments start with /** and end with */.
+
+// Java Data Types:
+// 1. Primitive Data Types: byte, short, int, long, float, double, boolean, char
+// 2. Non-Primitive Data Types: String, Arrays, Classes, Interfaces
+
+// Java Variables:
+// 1. Variables are used to store data.
+// 2. Variables are declared using the data type.
+// 3. Variables are assigned using the assignment operator.
+// 4. Variables are initialized using the assignment operator.
+// 5. Variables are accessed using the variable name.
