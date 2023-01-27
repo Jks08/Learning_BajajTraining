ed726cce04fb057aa252c943b14e57b820f21275
Jishnu Kumar Srivastava <jisheras31@gmail.com>
2023-01-27 11:12:52 +0530

    Update test1.py

--- Python/Flask/test1.py
+++ Python/Flask/test1.py
@@ -6,6 +6,11 @@ app = Flask(__name__)
 def hello_world():
     return '<h1> Hello User! Welcome to Flask! </h1>' #Jinja2 will be used to render HTML. It understands HTML tags.
 
+@app.route('/index')
+def index():
+    browser_info = request.headers.get('User-Agent')
+    return '<h2> Your browser is %s </h2>' % browser_info
+
 @app.route('/user/<name>')
 def add(name):
     return '<p> Hello %s' % name
