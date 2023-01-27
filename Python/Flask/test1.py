76b114034fbcf2e5ecab31058dcc85b63d7d2e89
Jishnu Kumar Srivastava <jisheras31@gmail.com>
2023-01-27 11:05:57 +0530

    Learning Flask.

---
+++ Python/Flask/test1.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return '<h1> Hello User! Welcome to Flask! </h1>' #Jinja2 will be used to render HTML. It understands HTML tags.
+
+@app.route('/user/<name>')
+def add(name):
+    return '<p> Hello %s' % name
+
+if __name__=='__main__':
+    app.run(debug=True)   
