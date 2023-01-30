01f4f8f00b4fdef126460a2c213d1af3a26b1d9c
Jishnu Kumar Srivastava <jisheras31@gmail.com>
2023-01-30 10:32:43 +0530

    more changes.

--- Python/Flask/Basics1.py
+++ Python/Flask/Basics1.py
@@ -1,11 +1,15 @@
 # Basic html template rendering in Flask
 
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('Basic1.html')
+
+# wtforms and wtf in flask
 
 if __name__ == '__main__':
     app.run(debug=True)
