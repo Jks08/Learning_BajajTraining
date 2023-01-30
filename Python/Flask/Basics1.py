71c21a24acbc9780ec203d124bfb28533e683edf
Jishnu Kumar Srivastava <jisheras31@gmail.com>
2023-01-30 10:52:47 +0530

    WTForms and CSRF Protection

--- Python/Flask/Basics1.py
+++ Python/Flask/Basics1.py
@@ -2,14 +2,26 @@
 
 from flask import Flask, render_template
 from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+# wtforms and wtf in flask
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'mysecretkey'
 
-@app.route('/')
+class TestForm(FlaskForm):
+    cat_breed = StringField('Enter your cat breed: ')
+    submit = SubmitField('Submit')
+
+@app.route('/',methods=['GET','POST'])
 def index():
-    return render_template('Basic1.html')
+    cat_breed = False
+    form = TestForm()
 
-# wtforms and wtf in flask
+    if(form.validate_on_submit()):
+        cat_breed = form.cat_breed.data
+        form.cat_breed.data = ''
+
+    return render_template('catBreed.html', cat_breed=cat_breed, form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
