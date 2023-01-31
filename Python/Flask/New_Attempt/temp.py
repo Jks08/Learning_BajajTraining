264d5dabe6c63094d112ad4fa21bd3fa9fba4efa
Jishnu Kumar Srivastava <jisheras31@gmail.com>
2023-01-31 16:43:33 +0530

    del temp

--- Python/Flask/New_Attempt/temp.py
+++
@@ -1,8 +0,0 @@
-import psycopg2
-
-con = psycopg2.connect("dbname=Jks_Try1 user=postgres password=Finserv@2023")
-cur = con.cursor()
-
-cur.execute("SELECT * FROM pet_regs")
-data = cur.fetchall()
-print(data)
