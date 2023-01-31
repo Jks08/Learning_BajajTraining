2e0fefe5e0b2b8bea21d0675f7988e029d175aca
Jishnu Kumar Srivastava <jisheras31@gmail.com>
2023-01-31 10:18:16 +0530

    Just some comments

--- Python/Flask/Postgresql_and_Flask.py
+++ Python/Flask/Postgresql_and_Flask.py
@@ -15,3 +15,20 @@ cur = con.cursor()
 cur.execute("select * from test;")
 print(cur.fetchall())
 con.commit()
+
+''' This is how I did flask db init'''
+
+# from flask import *
+# from flask_sqlalchemy import SQLAlchemy
+# from flask_migrate import Migrate
+
+# app = Flask(__name__)
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Finserv@2023@localhost:5432/Jks_Try1'
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# db = SQLAlchemy(app)
+# migrate = Migrate(app, db)
+
+''' Terminal commands: 1)pip3 install flask_migrate
+2)export FLASK_APP=Postgresql_and_Flask.py
+3) flask --help (chekc for db in the commands in this
+4) flask db init'''
